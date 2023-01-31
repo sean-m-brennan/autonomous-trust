a6e4d1fa1e70594b039f0e74d3065776f565d4c3
Sean Brennan <sean.brennan@tekfive.com>
2023-01-30 17:23:42 -0700

    Added optimization animation
    Former-commit-id: fd1b827b1a6e93af832b699a247805b407b1c1e8

--- doc/presentation/images/process_images.sh
+++ doc/presentation/images/process_images.sh
@@ -1,13 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-for file in file spreadsheet; do
+this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+cd "$this_dir" || exit
+for file in file spreadsheet demon devil neutral police smiley upset; do
     dia -e $file.shape $file.dia
 done
 
-for file in identity reputation; do
+for file in language negotiation identity reputation optimization; do
     dia -e $file.svg $file.dia
 done
 
 for file in network; do
     dia -e $file.png $file.dia
 done
+
+cp optimization.svg optimization.svg.tmp
+cat optimization.svg.tmp | head -n 2 > optimization.svg
+echo '<?xml-stylesheet type="text/css" href="../css/optimization.css"?>' >> optimization.svg
+cat optimization.svg.tmp | tail -n +3 >> optimization.svg
+rm -f optimization.svg.tmp
