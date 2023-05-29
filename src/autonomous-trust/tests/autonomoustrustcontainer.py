f225027138870929005ffa91dd4fb9345dca0009
Sean Brennan <sean.brennan@tekfive.com>
2023-05-29 14:00:25 -0600

    Struggling with dockerizing tests
    Former-commit-id: 9c0880a505f000ec3648a2d736670562d4baa0aa

--- src/autonomous-trust/tests/autonomoustrustcontainer.py
+++ src/autonomous-trust/tests/autonomoustrustcontainer.py
@@ -1,15 +1,13 @@
 import os
-import time
 import docker
-from autonomous_trust.core.system import comm_port
-from autonomous_trust.core.network.ping import ping
+import autonomous_trust
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.waiting_utils import wait_container_is_ready, wait_for_logs
 
 network_name = 'at-net'
-image = 'autonomous-trust'
 network = '172.27.3.0'
 mask = 24
+host_dir = os.path.abspath(os.path.join(os.path.dirname(autonomous_trust.__file__), '..'))
 
 
 def docker_init():
@@ -23,13 +21,12 @@ def docker_init():
 
 
 class AutonomousTrustContainer(DockerContainer):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, image='autonomous-trust', **kwargs) -> None:
         super().__init__(image=image + ':latest', **kwargs)
-        self.port = comm_port
         args = '--test'
         if 'tests' in kwargs:
             args = args + ' ' + kwargs['tests']
-        self.with_env('AUTONOMOUS_TRUST_TEST_ARGS', args)
+        self.with_env('AUTONOMOUS_TRUST_ARGS', args)
         self.with_kwargs(remove=True, network=network_name)
         self.ip_address = None
 
@@ -41,12 +38,24 @@ class AutonomousTrustContainer(DockerContainer):
             if 'Bound peer recv to' in line:
                 self.ip_address = line.split()[-1].split(':')[0]
                 break
-        time.sleep(5)
-        #print(self.get_logs()[0].decode())
-        stats = ping(self.ip_address)  # FIXME fails - blocked by docker?
-        print(stats)
+        #with open(os.path.join(host_dir, 'docker_ips'), 'a') as ip:
+        #    ip.write(self.ip_address + '\n')
 
     def start(self) -> "AutonomousTrustContainer":
         super().start()
         self.readiness_probe()
         return self
+
+
+class AutonomousTrustTestContainer(AutonomousTrustContainer):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(image='autonomous-trust-test' + ':latest', **kwargs)
+        args = '--test'
+        if 'tests' in kwargs:
+            args = args + ' ' + kwargs['tests']
+        self.with_env('AUTONOMOUS_TRUST_TEST_ARGS', args)
+        self.volumes[host_dir] = '/app'
+
+    @wait_container_is_ready(TimeoutError)
+    def readiness_probe(self) -> None:
+        wait_for_logs(self, lambda x: 'AutonomousTrust' in x)  # FIXME
